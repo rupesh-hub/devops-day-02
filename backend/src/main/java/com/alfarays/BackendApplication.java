a0a73647bd68206413df2da65539163a00e54f52
rupesh672 <=>
2025-01-27 12:19:17 +0100

    feat: implemented ci

--- backend/src/main/java/com/alfarays/BackendApplication.java
+++ backend/src/main/java/com/alfarays/BackendApplication.java
@@ -2,12 +2,24 @@ package com.alfarays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping
+	public ResponseEntity<String> get(final Authentication authentication){
+		return new ResponseEntity<>(format("Hi, welcome to your -devops journey %s.", authentication.getName()), HttpStatus.OK);
+	}
 }
